9af1e20fb79f96d46216fb28b6849f5968b865d8
oblique <psyberbits@gmail.com>
2020-12-11 23:51:48 +0200

    Simplify day11

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -23,121 +23,65 @@ fn parse_layout() -> Result<Vec<Vec<Cell>>> {
         .collect()
 }
 
-fn adjacent_occupied_seats(
+fn count_occupied_seats(
     layout: &Vec<Vec<Cell>>,
     x: usize,
     y: usize,
+    limit: Option<usize>,
 ) -> usize {
-    let start_row = if x > 0 {
-        x - 1
-    } else {
-        x
-    };
-
-    let start_col = if y > 0 {
-        y - 1
-    } else {
-        y
-    };
-
-    let end_row = (x + 1).min(layout.len() - 1);
-    let end_col = (y + 1).min(layout[0].len() - 1);
-
-    let mut count = 0;
-
-    for i in start_row..=end_row {
-        for j in start_col..=end_col {
-            if i == x && j == y {
-                continue;
-            }
-
-            if layout[i][j] == Cell::OccupiedSeat {
-                count += 1;
-            }
-        }
-    }
-
-    count
-}
-
-fn visible_occupied_seats(
-    layout: &Vec<Vec<Cell>>,
-    x: usize,
-    y: usize,
-) -> usize {
-    let mut direction_finished = [[false; 3]; 3];
-
     #[rustfmt::skip]
     let directions = [
-        [(-1, -1), (-1, 0), (-1, 1)],
-        [ (0, -1),  (0, 0),  (0, 1)],
-        [ (1, -1),  (1, 0),  (1, 1)],
+        (-1, -1), (-1, 0), (-1, 1),
+         (0, -1),           (0, 1),
+         (1, -1),  (1, 0),  (1, 1),
     ];
 
     let mut count = 0;
 
-    for n in 1..layout.len().max(layout[0].len()) {
-        for i in 0..3 {
-            for j in 0..3 {
-                if i == 1 && j == 1 {
-                    continue;
-                }
+    for (dir_x, dir_y) in &directions {
+        let mut n = 0;
 
-                if direction_finished[i][j] {
-                    continue;
-                }
+        while limit.map_or(true, |x| n < x) {
+            n += 1;
 
-                let (dir_x, dir_y) = directions[i][j];
+            let x2 = x as isize + (n as isize * dir_x);
+            let y2 = y as isize + (n as isize * dir_y);
 
-                let x2 = x as isize + (n as isize * dir_x);
-                let y2 = y as isize + (n as isize * dir_y);
+            if x2 < 0
+                || y2 < 0
+                || x2 as usize >= layout.len()
+                || y2 as usize >= layout[0].len()
+            {
+                break;
+            }
 
-                if x2 < 0
-                    || y2 < 0
-                    || x2 as usize >= layout.len()
-                    || y2 as usize >= layout[0].len()
-                {
-                    direction_finished[i][j] = true;
-                    continue;
-                }
-
-                match layout[x2 as usize][y2 as usize] {
-                    Cell::Floor => {}
-                    Cell::EmptySeat => {
-                        direction_finished[i][j] = true;
-                    }
-                    Cell::OccupiedSeat => {
-                        count += 1;
-                        direction_finished[i][j] = true;
-                    }
+            match layout[x2 as usize][y2 as usize] {
+                Cell::Floor => {}
+                Cell::EmptySeat => break,
+                Cell::OccupiedSeat => {
+                    count += 1;
+                    break;
                 }
             }
-        }
-
-        let count_finished: usize = direction_finished
-            .iter()
-            .map(|row| row.iter().filter(|&&x| x).count())
-            .sum();
-
-        if count_finished == 8 {
-            break;
         }
     }
 
     count
 }
 
-fn round<F>(layout: &mut Vec<Vec<Cell>>, tolerance: usize, count: F) -> bool
-where
-    F: Fn(&Vec<Vec<Cell>>, usize, usize) -> usize,
-{
+fn round(
+    layout: &mut Vec<Vec<Cell>>,
+    tolerance: usize,
+    limit: Option<usize>,
+) -> bool {
     let mut seat_changed = false;
     let saved_layout = layout.clone();
 
     for i in 0..layout.len() {
         for j in 0..layout[0].len() {
             let cur_seat = saved_layout[i][j];
-            let occupied_seats = count(&saved_layout, i, j);
+            let occupied_seats =
+                count_occupied_seats(&saved_layout, i, j, limit);
 
             if cur_seat == Cell::EmptySeat && occupied_seats == 0 {
                 layout[i][j] = Cell::OccupiedSeat;
@@ -158,11 +102,7 @@ fn solve_part1() -> Result<()> {
     let mut layout = parse_layout()?;
 
     loop {
-        let seat_changed = round(&mut layout, 4, |layout, i, j| {
-            adjacent_occupied_seats(layout, i, j)
-        });
-
-        if !seat_changed {
+        if !round(&mut layout, 4, Some(1)) {
             break;
         }
     }
@@ -183,11 +123,7 @@ fn solve_part2() -> Result<()> {
     let mut layout = parse_layout()?;
 
     loop {
-        let seat_changed = round(&mut layout, 5, |layout, i, j| {
-            visible_occupied_seats(layout, i, j)
-        });
-
-        if !seat_changed {
+        if !round(&mut layout, 5, None) {
             break;
         }
     }
